d094cefc53eda49620fc2aea5535a5f4c79b73a9
Surace222 <suresh.tamang163@gmail.com>
2025-08-14 18:57:18 +0545

    some styling as well as whole logice of the game in javascript is added

--- app.js
+++ app.js
@@ -1,10 +1,73 @@
 let boxes = document.querySelectorAll(".box");
 let turnO= true; // player are playerX and playerO
+let msgbox=document.querySelector(".msg-container");
+let newButton =document.querySelector("#new-btn");
+let msgContainer= document.querySelector(".msg-container");
+let msg=document.querySelector("#message");
+let resetButton=document.querySelector("#reset");
+
 const winPattern=[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
+const resetGame=()=>{
+  turnO=true;
+  enableBoxs();
+  msgContainer.classList.add("hide");
+
+};
+
 //adding event listner in each boxes or button
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-      console.log("box was clicked");
-    })
+boxes.forEach((box)=>{
+  box.addEventListener("click", () =>{
+    console.log("the box was clicked")
+    if(turnO===true){
+      box.innerText="O";
+      turnO=false;
+    }else{
+      box.innerText="X";
+      turnO=true;
+    }
+    checkWinner();
+    box.disabled="true";
+    
+  })
 })
+
+const disableBoxs = () =>{
+  for(let box of boxes){
+    box.disabled=true;
+  }
+}
+
+const enableBoxs = () =>{
+  for(let box of boxes){
+    box.disabled=false;
+    box.innerText="";
+  }
+}
+
+const showWinner=(winner)=>{
+    msg.innerText =`Congratulatin winnner is ${winner}`
+    msgContainer.classList.remove("hide");
+    disableBoxs();
+}
+
+const checkWinner=()=>{
+  for (let pattern of winPattern){
+   let pos1val=boxes[pattern[0]].innerText;
+   let pos2val= boxes[pattern[1]].innerText;
+   let pos3val= boxes[pattern[2]].innerText;
+
+   if(pos1val !="" && pos2val !=""  && pos3val !="" ){
+    if (pos1val===pos2val && pos2val === pos3val){
+      console.log("winner", pos1val )
+
+      showWinner(pos1val);
+    }
+     
+   }
+    
+  }
+}
+
+newButton.addEventListener("click", resetGame);
+resetButton.addEventListener("click", resetGame );
